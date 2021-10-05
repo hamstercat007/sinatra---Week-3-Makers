5c1592b5e502cd1d455d48f9d488e3cdf14116af
Lisa Mac <lisa@MacBook-Air-2.local>
2021-10-05 18:01:56 +0100

    POST ed params

--- app.rb
+++ app.rb
@@ -18,10 +18,22 @@ get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
 end 
 
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  @surname = params[:surname]
+  @color = params[:color]
+  erb :index
+end 
+
 get '/named-cat' do
   p params
   @name = params[:name]
   @surname = params[:surname]
   @color = params[:color]
   erb :index
+end 
+
+get '/cat-form' do 
+  erb :cat_form
 end 
